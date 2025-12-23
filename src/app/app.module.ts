0bbcf09b6e88d4110931f356d482d0aab74801bc
CJC803 <cjoshcummin@hotmail.com>
2025-12-23 00:50:16 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 import { KnowledgeChartsComponent } from './components/knowledge-charts/knowledge-charts.component';
 import { RouteBaselineComponent } from './components/route-baseline/route-baseline.component';
 import { DriverBaselineComponent } from './components/driver-baseline/driver-baseline.component';
 import { ComparisonStaffingComponent } from './components/comparison-staffing/comparison-staffing.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'knowledge-charts', pathMatch: 'full' },
-  {
-    path: 'knowledge-charts',
-    component: KnowledgeChartsComponent,
-    children: [
-      { path: '', redirectTo: 'routes', pathMatch: 'full' },
-      { path: 'routes', component: RouteBaselineComponent },
-      { path: 'drivers', component: DriverBaselineComponent },
-      { path: 'comparison', component: ComparisonStaffingComponent }
-    ]
-  },
-  { path: '**', redirectTo: 'knowledge-charts' }
-];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+    AppComponent,
+    KnowledgeChartsComponent,
+    RouteBaselineComponent,
+    DriverBaselineComponent,
+    ComparisonStaffingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppRoutingModule {}
+export class AppModule {}
