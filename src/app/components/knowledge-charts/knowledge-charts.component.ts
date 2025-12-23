488fae2e9c23b6285217ff97d35c89b5feed6cee
CJC803 <cjoshcummin@hotmail.com>
2025-12-23 00:45:30 -0500

    Create knowledge-charts.component.ts

---
+++ src/app/components/knowledge-charts/knowledge-charts.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-knowledge-charts',
+  templateUrl: './knowledge-charts.component.html',
+  styleUrls: ['./knowledge-charts.component.css']
+})
+export class KnowledgeChartsComponent {}
